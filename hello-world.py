ac1d8c3d0e9c1793bd4c6dc1d8d6544b7aefe750
itolf <itolf@users.noreply.github.com>
2022-07-12 11:19:24 +0100

    Delete hello-world.py

--- hello-world.py
+++
@@ -1,4 +0,0 @@
-# This is a well used first coding program that displays a text output
-
-print('Hello World')  # prints the string 'Hello World' to screen
-print('It is a lovely day today')  # prints another string of text
