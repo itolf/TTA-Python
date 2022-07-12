1f656361c36fce46058bd72313d85207e50c37db
itolf <itolf@users.noreply.github.com>
2022-07-12 15:16:38 +0100

    Add files via upload

---
+++ wk04-hlt-going-loopy.py
@@ -0,0 +1,10 @@
+# Ian Tolfrey
+# 05-07-2022
+# This program uses a while loop and takes user input until total reaches 50
+
+total=0
+
+while total<=50:
+    num1=int(input("Please enter a number: \n"))
+    total=total+num1
+    print(total)
