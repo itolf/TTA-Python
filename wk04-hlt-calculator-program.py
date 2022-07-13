9d9d4e5a925abe8edb206c18386e231b8849c48e
itolf <itolf@users.noreply.github.com>
2022-07-13 17:03:06 +0100

    Add files via upload

--- wk04-hlt-calculator-program.py
+++ wk04-hlt-calculator-program.py
@@ -2,12 +2,13 @@
 # 12-07-2022
 # This program run calulations for the user from a menu
 
+# Subprogram to get user input and call correct Subprogram for the calculation
 def calc():
-    global userNumber1
+    global userNumber1 # decalring the global variables
     global userNumber2
 
     print("\n\t====>>>> MENU-CALC <<<<====\n")
-    choice=input("What calculation would you like to do? \n\n1> Addition: \n2> Subtraction: \n3> Division: \n4> Multiplication: \n")
+    choice=input("What calculation would you like to do? \n\n1> Addition: \n2> Subtraction: \n3> Division: \n4> Multiplication: \n") # displaying calculator options
     userNumber1=int(input("Please enter your first number: "))
     userNumber2=int(input("Please enter your second number: "))
     if choice=="1":
@@ -19,20 +20,20 @@ def calc():
     else:
         multiplication()
 
-def addition():
+def addition(): # addition subprogram
     sum=userNumber1+userNumber2
     print("The answer is: ",sum)
 
-def subtraction():
+def subtraction(): # subtraction subprogram
     sum=userNumber1-userNumber2
     print("The answer is: ",sum)
 
-def division():
+def division(): # division subprogram
     sum=userNumber1/userNumber2
     print("The answer is: ",sum)
 
-def multiplication():
+def multiplication(): # multiplication subprogram
     sum=userNumber1*userNumber2
     print("The answer is: ",sum)
 
-calc()
+calc() # calling the subprogram calc to run
