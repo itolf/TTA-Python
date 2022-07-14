365c0d7fd53c1b641237d1195b7261e51ee512e2
itolf <itolf@users.noreply.github.com>
2022-07-14 01:25:28 +0100

    Add files via upload

--- wk05-string-manipulation.py
+++ wk05-string-manipulation.py
@@ -1,14 +1,16 @@
 # Ian Tolfrey
 # 13/07/2022
 # String manipulation and validation
-word="Welcome to TTA" # string
+
+word=input("\nPlease enter a string of words: ") # requesting user entered string
+
 print("\nOrginal word is ... ",word) # prints the string
 print("\nUpper case :",word.upper()) # prints string in upper case
 print("\nLower case :",word.lower()) # prints string in lower case
 print("\nCapitalised :",word.capitalize()) # capitalises the first word
 print("\nTitled words :",word.title()) # capitalises each word in the string
 print("\nWord count :",word.count("e"),"in this word") # prints how many words in the string
-print("\nFind word :",word.find("l")) # prints how many of a letter appear in the string
+print("\nFind word :",word.find("c")) # prints how many of a letter appear in the string
 print("\nLength of word \n",len(word)) # prints total length of the string including spaces
 print() # blank line
 print(word.isdigit()) # checks the string for numberical characters
