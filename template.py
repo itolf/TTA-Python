d6902dae2b8f2aef80fd82eaec53fe19565606b5
itolf <itolf@users.noreply.github.com>
2022-07-16 15:20:33 +0100

    Add files via upload

---
+++ template.py
@@ -0,0 +1,11 @@
+'''
+Ian Tolfrey
+date
+description
+'''
+
+print ('\033[40m' + "\n\t<<<<••••  TITLE  ••••>>>>\n" + '\033[0m')
+
+
+
+print('\033[40m' + "\n\t<<•• END OF PROGRAM ••>>\n" + '\033[0m')
