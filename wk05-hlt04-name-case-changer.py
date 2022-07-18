31c02d2fa6c70a8facca4e50cd68b5dc32ef5512
itolf <itolf@users.noreply.github.com>
2022-07-18 14:54:04 +0100

    Add files via upload

--- wk05-hlt04-name-case-changer.py
+++ wk05-hlt04-name-case-changer.py
@@ -6,6 +6,12 @@ description
 
 print ('\033[40m' + "\n\t<<<<••••  NAME CASE CHANGER  ••••>>>>\n" + '\033[0m')
 
+firstName=input("Please enter your name: ")#
 
+if len(firstName)<5:
+    lastName=input("Please enter your surname: ")
+    print(firstName.upper(), lastName.upper())
+else:
+    print(firstName.lower())
 
-print('\033[40m' + "\n\t<<•• END OF PROGRAM ••>>\n" + '\033[0m')
+print('\033[40m' + "\n\t\t<<•• END OF PROGRAM ••>>\n" + '\033[0m')
